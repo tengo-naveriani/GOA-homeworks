74ea04a6b137b44966895544ffd5dc047de730a4
tengo-naveriani <tengonaveriani3@gmail.com>
2024-07-06 17:53:27 +0400

    forth commit

---
+++ day 4/classwork.py
@@ -0,0 +1,15 @@
+city- 'Tbilisi'
+print (city)
+#tires nacvlad udrisi unda eweros
+number=(5)
+print(number
+#frcxilebi ar aris daxuruli
+name=nika
+print(nika)
+#nika frcxilebsi unda eweros
+name-'nika'
+print(name)
+#tires nacvlad udrisi unda eweros
+number-'7'
+print("vashli)
+#frcxili aklia
