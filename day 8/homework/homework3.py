97efd0a253aa7e4e5c4482e2f31e172634123d07
tengo-naveriani <tengonaveriani3@gmail.com>
2024-07-26 21:34:11 +0800

    second commit

---
+++ day 8/homework/homework3.py
@@ -0,0 +1,8 @@
+# მომხმარებელს შემოვატანინე ასაკი და სიმაღლე
+height=int(input('enter your height'))
+age=int(input('enter your age'))
+# შევასრულე არითმეტიკული მოქმედებები ასაკსა და სიმაღლეზე
+print(height/age)
+print(height*age)
+print(height+age)
+print(height-age)
